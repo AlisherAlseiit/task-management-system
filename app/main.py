17d3e1fc2af68febebc7e2e8f4a7dcac1aa7f265
Alish <magawin51@gmail.com>
2023-04-25 18:35:49 +0600

    modified files

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from .routers import user, auth
 
 app = FastAPI()
 
@@ -15,6 +16,9 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.include_router(user.router)
+app.include_router(auth.router)
 
 
 @app.get("/")

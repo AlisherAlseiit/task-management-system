b2826310304f1b83cf0bfc4940b01426ca617fc0
Alish <magawin51@gmail.com>
2023-04-26 15:50:39 +0600

    added new pydantic models, to validate endpoint I\O

--- app/schemas.py
+++ app/schemas.py
@@ -21,6 +21,11 @@ class UserOut(UserBase):
         orm_mode = True
 
 
+class User(UserBase):
+    class Config:
+        orm_mode = True
+
+
 class Token(BaseModel):
     access_token: str
     token_type: str
@@ -28,5 +33,44 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     email: str | None = None
+
+
+class BoardBase(BaseModel):
+    name: str
+    
+
+class BoardCreate(BoardBase):
+    pass
+
+
+class ListBase(BaseModel):
+    name: str
+    position: int
+
+
+class ListCreate(ListBase):
+    pass
+
+
+class ListOut(ListBase):
+    id: int
+    created_at: datetime
+    board_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class BoardOut(BoardBase):
+    id: int
+    owner_id: int
+    created_at: datetime
+
+    owner: User
+    lists: list[ListOut]
+
+    class Config:
+        orm_mode = True
+
 
 
